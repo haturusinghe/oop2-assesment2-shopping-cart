1340f6bc3dc08f24cb148f45eb7b74649d973360
S.Haturusinghe <haturusingh3@gmail.com>
2022-10-02 21:05:20 +0530

    added crud functions

--- src/main/java/cs85/oop2/assignment2/controller/SalesItemViewController.java
+++ src/main/java/cs85/oop2/assignment2/controller/SalesItemViewController.java
@@ -1,10 +1,11 @@
 package cs85.oop2.assignment2.controller;
 
+import cs85.oop2.assignment2.model.SaleItem;
 import cs85.oop2.assignment2.service.SaleItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SalesItemViewController {
@@ -19,5 +20,37 @@ public class SalesItemViewController {
     public String itemList(Model model){
         model.addAttribute("items",saleItemService.getSaleItems());
         return "item_list";
+    }
+
+    @GetMapping("/add")
+    public String addItem(Model model) {
+        SaleItem saleItem = new SaleItem();
+        model.addAttribute("saleItem", saleItem);
+        return "add_item";
+
+    }
+
+    @PostMapping("/add")
+    public String saveItem(@ModelAttribute("saleItem") SaleItem saleItem) {
+        saleItemService.addNewSaleItem(saleItem);
+        return "redirect:/";
+    }
+
+    @GetMapping("/item/{id}")
+    public String editSaleItem(@PathVariable Long id, Model model) {
+        model.addAttribute("saleItem", saleItemService.getSaleItem(id).get());
+        return "edit_item";
+    }
+
+    @PostMapping("/item/{id}")
+    public String updateSaleItem(@PathVariable Long id,@ModelAttribute("saleItem") SaleItem saleItem ,Model model) {
+        saleItemService.addNewSaleItem(saleItem);
+        return "redirect:/";
+    }
+
+    @GetMapping("/item/delete/{id}")
+    public String deleteSaleItem(@PathVariable Long id) {
+        saleItemService.deleteSaleItem(id);
+        return "redirect:/";
     }
 }
