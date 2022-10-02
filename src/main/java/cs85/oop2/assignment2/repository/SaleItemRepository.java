ca0f746fda04953f75c1543d526ff8b72d0fd072
S.Haturusinghe <haturusingh3@gmail.com>
2022-10-02 16:51:52 +0530

    added derived query to find one item

--- src/main/java/cs85/oop2/assignment2/repository/SaleItemRepository.java
+++ src/main/java/cs85/oop2/assignment2/repository/SaleItemRepository.java
@@ -4,6 +4,12 @@ import cs85.oop2.assignment2.model.SaleItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SaleItemRepository extends JpaRepository<SaleItem,Long> {
+//    Optional<SaleItem> findByEmailContains(String email);
+
+    Optional<SaleItem> findOneByItemNumber(String itemNumber);
 }
