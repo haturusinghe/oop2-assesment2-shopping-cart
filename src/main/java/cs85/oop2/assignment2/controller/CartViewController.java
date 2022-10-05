4e1f2c07699b2696d95837618ededcb1315cebb1
S.Haturusinghe <haturusingh3@gmail.com>
2022-10-05 20:59:23 +0530

    added add to cart related methods

--- src/main/java/cs85/oop2/assignment2/controller/CartViewController.java
+++ src/main/java/cs85/oop2/assignment2/controller/CartViewController.java
@@ -1,21 +1,27 @@
 package cs85.oop2.assignment2.controller;
 
+import cs85.oop2.assignment2.model.CartItem;
+import cs85.oop2.assignment2.model.SaleItem;
 import cs85.oop2.assignment2.service.CartItemService;
+import cs85.oop2.assignment2.service.SaleItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(path = "cart")
 public class CartViewController {
     private final CartItemService cartItemService;
+    private final SaleItemService saleItemService;
 
     @Autowired
-    public CartViewController(CartItemService cartItemService) {
+    public CartViewController(CartItemService cartItemService, SaleItemService saleItemService) {
         this.cartItemService = cartItemService;
+        this.saleItemService = saleItemService;
     }
+
+
 
     @GetMapping
     public String viewCart(Model model){
@@ -23,6 +29,22 @@ public class CartViewController {
         model.addAttribute("citems",cartItemService.getAll());
         model.addAttribute("cmeta",cartMeta);
         return "cart";
+    }
+
+    @GetMapping("/add/{id}")
+    public String addItemToCart(Model model,@PathVariable Long id){
+        CartItem itemToAdd = new CartItem();
+        itemToAdd.setItem(saleItemService.getSaleItem(id).get());
+        model.addAttribute("itemToAdd", itemToAdd);
+        return "add_cart";
+    }
+
+    @PostMapping("/add/{id}")
+    public String addItemToCart(@ModelAttribute("itemToAdd") CartItem cartItem , Model model, @PathVariable Long id){
+        SaleItem saleItem = saleItemService.getSaleItem(id).get();
+        CartItem itemToAdd = new CartItem(saleItem,cartItem.getCartons(),cartItem.getSingleItems());
+        cartItemService.addItemToCart(itemToAdd);
+        return "redirect:/cart";
     }
 
     private class CartMeta {
